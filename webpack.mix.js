1596088d980d338177b7005812b8baf2627ba801
morpheus <morpheusadam95@gmail.com>
2024-12-19 18:53:09 +0330

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,11 +5,43 @@ const mix = require('laravel-mix');
  | Mix Asset Management
  |--------------------------------------------------------------------------
  |
+ aso,+98 910 688 0593
+ehs,+98 912 045 9803
+ash,+98 918 999 8168
+maji,+98 918 738 7314
+
+aso,+98 910 688 0593
+ehs,+98 912 045 9803
+ash,+98 918 999 8168
+maji,+98 918 738 7314
+
+aso,+98 910 688 0593
+ehs,+98 912 045 9803
+ash,+98 918 999 8168
+maji,+98 918 738 7314
+
+aso,+98 910 688 0593
+ehs,+98 912 045 9803
+ash,+98 918 999 8168
+maji,+98 918 738 7314
+6277601401999303
+aso,+98 910 688 0593
+ehs,+98 912 045 9803
+ash,+98 918 999 8168
+maji,+98 918 738 7314
+
+aso,+98 910 688 0593
+ehs,+98 912 045 9803
+ash,+98 918 999 8168
+maji,+98 918 738 7314
+
+
  | Mix provides a clean, fluent API for defining some Webpack build steps
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
+
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
