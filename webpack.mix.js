89100fdbefbee6a826b35a4583fcadde96860aae
morpheus <morpheusadam95@gmail.com>
2024-12-22 18:50:24 +0330

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,7 +19,37 @@ aso,+98 910 688 0593
 ehs,+98 912 045 9803
 ash,+98 918 999 8168
 maji,+98 918 738 7314
+Skip to main content
+Department of State Seal
+U.S. Department of State
+Bureau of Consular Affairs
+Electronic Diversity Visa
+Applicant Entry System: 12.11
+DV-2026 Submission Confirmation: Entry Received
+Success!
 
+Your entry for the 2026 Diversity Visa program was received on Tuesday, October 8, 2024 at 9:00:29 AM EDT. Please do NOT close this window until you have printed this confirmation page or made a record of your Confirmation Number.
+Entrant Name:
+AHMADPOUR, HESAM ALDIN
+Confirmation Number:
+20263QN417L91OQB
+Year of Birth:
+1997
+Digital Signature:
+2930AB1A4067F4EDAFE4337FF02FACC2CEDE3C01
+
+Thank you for your entry for the 2026 Diversity Visa Program.
+
+Please either print this page or make a record of the confirmation number before closing this window. You will not be able to retrieve this number after you close this window.
+
+You must retain your confirmation number in order to check your entry status via Entrant Status Check between May 3, 2025 and September 30, 2026 to determine whether your entry was selected for further processing in the 2026 Diversity Visa Program. You will be REQUIRED to enter your confirmation number in combination with other personal information in order to check on your entry status.
+
+Selectees will not receive selectee notifications or letters by regular postal mail from the Kentucky Consular Center (KCC).
+
+Do not submit additional Entry Forms with this person as the Primary Entrant! Multiple entries will disqualify the Entrant from participation in the 2026 Diversity Visa Program.
+USA Gov
+
+This site is managed by the Bureau of Consular Affairs, U.S. Department of State. External links to other Internet sites should not be construed as an endorsement of the views contained therein.
 aso,+98 910 688 0593
 ehs,+98 912 045 9803
 ash,+98 918 999 8168
